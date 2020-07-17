e6e02a9381a3575d2d27fcfe474e91ffc71903e5
butcher313 <butcher_313@naver.com>
2020-07-17 11:00:57 +0900

    commit

--- moneyswag/app.py
+++ moneyswag/app.py
@@ -8,6 +8,7 @@ from bson import ObjectId
 
 from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+#client = MongoClient('mongodb://ho1920:foavkq250@localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
 
 ############################로그인에 필요한 패키지 등
